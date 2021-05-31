b5ea28a2debe6ba3a50f05c3f3a408a7a910b8cc
jyh4479 <jyh.dev@gmail.com>
2021-05-31 23:49:41 +0900

    update redux

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react'
 import { Card } from '../components'
 
 import { useDispatch, useSelector } from 'react-redux';
-import { countPlusAction, countMinusAction, userListAction } from '../reducers/count'
+import { userListAction, userKeyAction } from '../reducers/count'
 
 
 const axios = require('axios')
@@ -10,28 +10,37 @@ const axios = require('axios')
 const CardColor = ['#FF6666', '#3366CC', '#FFFF66', '#555555', '#33CC33', '#FF9900']
 
 const CardContainer = props => {
-
-  const dispatch = useDispatch(); // dispatch를 사용하기 쉽게 하는 hook
-  // const count = useSelector(state => state.count.number); // store의 state를 불러오는 hook   store의 state 중에서 count의 state를 불러온다.
-  const count = 0
-  // const data = useSelector(state => state.count.data); // store의 state를 불러오는 hook   store의 state 중에서 count의 state를 불러온다.
-  const data = 0
-  const userList = useSelector(state => state.count.userList); // store의 state를 불러오는 hook   store의 state 중에서 count의 state를 불러온다.
-
-  const onClickPlus = useCallback(() => { // useCallback은 최적화를 위한 hook이다 이 앱에선 굳이 사용 안 해도 되는데 습관이 들면 좋기에 사용.
-    dispatch(countPlusAction());
-  }, []);
-
-  const onClickMinus = useCallback(() => {
-    dispatch(countMinusAction());
-  }, []);
-  const onClickTest = useEffect(()=>{
-    dispatch(userListAction());
-  },[])
-
+  const dispatch = useDispatch();
+  const userList = useSelector(state => state.count.userList);
 
   const [cardData, setCardData] = useState([])
   const [viewData, setViewData] = useState([])
+  const [testData, setTestData] = useState([])
+
+  useEffect(()=>{
+    dispatch(userListAction());
+  },[dispatch])
+
+  useEffect(()=>{
+    setTestData(userList)
+  },[userList])
+
+  const onClickTest = () => {
+    console.log(userList)
+    console.log(testData)
+    mapTest(testData)
+  }
+  const onClickTest2 = () => {
+    console.log(userList)
+    console.log(testData)
+    mapTest(testData)
+  }
+
+  const mapTest = listData => {
+    listData.forEach((item)=>{
+      console.log(item['name'])
+    })
+  }
 
   const dataURL = props.dataURL
 
@@ -85,11 +94,8 @@ const CardContainer = props => {
       </div>
 
       <div>
-        <div>카운트 : {count}</div>
-        <div>데이터 : {data}</div>
-        <button onClick={onClickPlus}>+</button>
-        <button onClick={onClickMinus}>-</button>
         <button onClick={onClickTest}>++</button>
+        <button onClick={onClickTest2}>--</button>
       </div>
     </>
   )
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,76 +1,47 @@
 const axios = require('axios')
 
 export const initialState = {
-  number: 0,
-  data: 'HIHI',
   userList: [],
+  key:0,
 } // 처음 state값으로 count 0을 주었다. state값은 객체, 배열로도 사용할 수 있다.
 
-export const COUNT_PLUS = 'COUNT_PLUS' // count 1을 증가시킬 액션 타입이다.
-export const COUNT_MINUS = 'COUNT_MINUS' // count 1을 감소시킬 액션 타입이다.
 
-export const initialUserList = []
 export const GET_USER_LIST = 'GET_USER_LIST'
-export const CHANGE_KEY = 'CHANGE_KEY'
-
-const getUserList = async () => {
-  const userList = await axios.get('http://localhost:8000/UserList')
-  return {
-    number: 11,
-    data: 'USERLIST',
-    userList: userList.data,
-  }
-}
-
-export const countPlusAction = () => ({ // 액션 생성 함수
-  type: COUNT_PLUS
-})
-
-export const countMinusAction = () => ({
-  type: COUNT_MINUS
-})
+export const SET_USER_KEY = 'SET_USER_KEY'
 
 export const userListAction = () => {
   return async function (dispatch, getState) {
     const result = await axios.get(`http://localhost:8000/UserList`)
     dispatch({type:GET_USER_LIST, payload:result.data})
   }
-  // try{
-  //   const result = await axios.get(`http://jsonplaceholder.typicode.com/users`)
-  //   return{
-  //     type:GET_USER_LIST,
-  //     payload:result.data,
-  //   }
-  //
-  // } catch (e) {
-  //   return{
-  //     payload: console.log(e),
-  //   }
-  // }
 }
 
-const reducer = (state = initialState, action) => { // 리듀서
-  switch (action.type) {  // 액션의 type이 COUNT_PLUS일땐 state에 + 1 COUNT_MINUS 일 땐 state에 -1
-    case COUNT_PLUS:
-      const testData = 'PLUS!'
-      return {
-        ...state,
-        number: state.number + 1,
-        data: testData,
-        userList: [],
-      }
-    case COUNT_MINUS:
-      const testData2 = 'MINUS!'
-      return {
-        ...state,
-        number: state.number - 1,
-        data: testData2,
-        userList: [],
-      }
+export const userKeyAction = state => {
+  if(0==0){
+    return{
+      type:SET_USER_KEY,
+      payload:1,
+    }
+  }
+  else{
+    return{
+      type:SET_USER_KEY,
+      payload:0,
+    }
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case GET_USER_LIST:
       return {
         ...state,
         userList: action.payload,
+      }
+    case SET_USER_KEY:
+      return {
+        ...state,
+        key: action.payload,
       }
     default:
       return state
